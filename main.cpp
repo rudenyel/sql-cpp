e6bf85ced8c087fd4d017d96d969a841b333ade0
rudenyel <119690451+rudenyel@users.noreply.github.com>
2024-06-22 17:25:31 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,220 +1,174 @@
-#include <cstdio>
+
 #include <string>
+#include <sstream>
+#include <iostream>
+#include <functional>
 #include "main.h"
-#include "sqlite3-wrapper.h"
+#include "wrapper.h"
 
-// db.value
-constexpr const char* sql_check_book_table = "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'books'";
+using namespace std;
 
-//db.exucute
-constexpr const char* sql_create_table =
-        "CREATE TABLE IF NOT EXISTS books ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "title VARCHAR(127) CHECK(title != ''),"
-        "first_name VARCHAR(127) CHECK(first_name != ''),"
-        "last_name VARCHAR(127) CHECK(last_name != '')"
-        ")"
+constexpr auto queryCheckBookTable =
+    "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'books'";
+
+constexpr auto queryCreateTable =
+    "CREATE TABLE IF NOT EXISTS books ("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "title VARCHAR(127) CHECK(title != ''),"
+    "first_name VARCHAR(127) CHECK(first_name != ''),"
+    "last_name VARCHAR(127) CHECK(last_name != '')"
+    ")"
 ;
 
-constexpr const char* sql_list = "SELECT * FROM books";
-constexpr const char* sql_list_by_author = "SELECT * FROM books ORDER BY last_name, title";
-constexpr const char* sql_list_by_title = "SELECT * FROM books ORDER BY title, last_name";
-
-constexpr const char* sql_insert = "INSERT INTO books (title, first_name, last_name) VALUES (?, ?, ?)";
-
-constexpr const char* sql_find_by_author = "SELECT * FROM books WHERE last_name LIKE ?";
-constexpr const char* sql_find_by_title = "SELECT * FROM books WHERE title LIKE ?";
-
-constexpr const char* sql_delete_by_id = "DELETE FROM books WHERE id = ?";
-constexpr const char* sql_drop_table = "DROP TABLE IF EXISTS books";
-
-
+constexpr auto queryBooks = "SELECT * FROM books";
+constexpr auto queryBooksSortByAuthor = "SELECT * FROM books ORDER BY last_name, title";
+constexpr auto queryBooksSortByTitle = "SELECT * FROM books ORDER BY title, last_name";
+constexpr auto queryBooksFindByAuthor = "SELECT * FROM books WHERE last_name LIKE ?";
+constexpr auto queryBooksFindByTitle = "SELECT * FROM books WHERE title LIKE ?";
+constexpr auto queryBookAdd = "INSERT INTO books (title, first_name, last_name) VALUES (?, ?, ?)";
+constexpr auto queryBookDelete = "DELETE FROM books WHERE id = ?";
+constexpr auto queryBooksDropTable = "DROP TABLE IF EXISTS books";
 
 constexpr const char* menu[] = {
-        "0) List books",
-        "1) List books by author",
-        "2) List books by title",
-        "3) Add books",
-        "4) Find books by author",
-        "5) Find books by title",
-        "6) Delete book by id",
-        "X) Drop table and exit",
-        "Q) Quit"
+    "List books",
+    "List books (sort by author)",
+    "List books (sort by title)",
+    "Find books by author",
+    "Find books by title",
+    "Add book",
+    "Delete book",
+    "Drop books table and exit",
 };
 
-constexpr const char* prompt = "Select an action or Q to quit";
+function<void(SQLite& db)> execute[] = {
+    Books,
+    BooksSortByAuthor,
+    BooksSortByTitle,
+    BooksFindByAuthor,
+    BooksFindByTitle,
+    BookAdd,
+    BookDelete,
+    BooksDropTable,
+};
 
-const char* promptline(const char* prompt) {
-    static char buf[MAX_SMALL_STRING_LENGTH];
-    printf("%s > ", prompt);
-    memset(buf, 0, MAX_SMALL_STRING_LENGTH);
-    if (fgets(buf, MAX_SMALL_STRING_LENGTH, stdin) != NULL) {
-        buf[strcspn(buf, "\n")] = '\0'; // trim CR/LF
-    }
-    return buf;
+void TextMenu(SQLite& db) {
+    while(true) {
+        cout << endl;
+        cout << "Current database " << db.filename() << ":" << endl;
+        int count = 0;
+        for (const char* item : menu)
+            cout << count++ << ": " << item << endl;
+
+        string input;
+        int item;
+        cout << "Select an action or 'q' to quit > ";
+        while (true) {
+            getline(cin, input);
+            if (stringstream(input) >> item) {
+                if ((item >=0) && (item <=count)) break;
+            }
+            else
+                if (input == "q") exit(0);
+            cout << "Incorrect input, try again > ";
+        }
+
+        execute[item](db);
+    }    
 }
 
-void do_menu(SQLite& db) {
-    char response = 'x';
-    const char* buf = nullptr;
-    while (true) {
-        puts("");
-        printf("Current database %s:\n", db.filename());
-        // print the menu
-        for (const char* s : menu) {
-            puts(s);
-        }
-        // get the response
-        buf = promptline(prompt);
-        size_t bufsize = strnlen(buf, MAX_SMALL_STRING_LENGTH);
-        // one character only, please
-        if (bufsize != 1) {
-            puts("Input too long or empty");
-            continue;
-        }
-        response = buf[0];
-        // process the response
-        if (response >= 'a' && response <= 'z') {   // make it uppercase
-            response -= 0x20;
-        }
-        // check if we know this one
-        if (!strchr("0123456XQ", response)) {
-            puts("Invalid response");
-            continue;
-        }
-        // we can handle this one right here
-        if (response == 'Q') {
-            break;
-        }
-        // do it
-        do_jump(db, response);
-    }
-}
-
-void do_jump(SQLite& db, const char response) {
-    switch (response) {
-        case '0':
-            do_list(db);
-            break;
-        case '1':
-            do_list_by_author(db);
-            break;
-        case '2':
-            do_list_by_title(db);
-            break;
-        case '3':
-            do_add(db);
-            break;
-        case '4':
-            do_find_by_author(db);
-            break;
-        case '5':
-            do_find_by_title(db);
-            break;
-        case '6':
-            do_delete(db);
-            break;
-        case 'X':
-            do_drop_exit(db);
-            break;
-        default:
-            break;
-    }
-}
-
-void display_rows(SQLite& db) {
-    int count = 0;
+void DisplayData(SQLite& db) {
+    int rows = 0;
+    const int columns = db.column_count();
     for (const char** row = db.fetch_row(); row; row = db.fetch_row()) {
-        for (int i = 0; i < db.column_count(); ++i) {
-            printf("%s%s", row[i], (i < db.column_count() - 1) ? "; " : "\n");
-        }
-        count++;
+        for (int column = 0; column < columns; ++column)
+            cout << row[column] << ((column < columns - 1) ? "; " : "\n");
+        rows++;
     }
-    if (count == 0) {
-        puts("Table is empty");
-    }
+    if (rows == 0) cout << "Queryset is empty" << endl;
 }
 
-void do_list(SQLite& db) {
-    puts("List books:");
-    db.select(sql_list);
-    display_rows(db);
+void Books(SQLite& db) {
+    cout << "List books:" << endl;
+    db.select(queryBooks);
+    DisplayData(db);
 }
 
-void do_list_by_author(SQLite& db) {
-    puts("List books ordered by author:");
-    db.select(sql_list_by_author);
-    display_rows(db);
+void BooksSortByAuthor(SQLite& db) {
+    cout << "List books (sort by author):" << endl;
+    db.select(queryBooksSortByAuthor);
+    DisplayData(db);
 }
 
-void do_list_by_title(SQLite& db) {
-    puts("List books ordered by title:");
-    db.select(sql_list_by_title);
-    display_rows(db);
+void BooksSortByTitle(SQLite& db) {
+    cout << "List books (sort by title):" << endl;
+    db.select(queryBooksSortByTitle);
+    DisplayData(db);
 }
 
-void do_add(SQLite& db) {
-    const char* buf = nullptr;
+void BooksFindByAuthor(SQLite& db) {
+    string last_name;
+    cout << "Find books by author last name > ";
+    getline(cin, last_name);
 
-    char title[MAX_SMALL_STRING_LENGTH];
-    char first_name[MAX_SMALL_STRING_LENGTH];
-    char last_name[MAX_SMALL_STRING_LENGTH];
+    db.select(queryBooksFindByAuthor, last_name.data());
+    DisplayData(db);
+}
 
-    puts("Add book:");
-    buf = promptline("Title");
-    strncpy(title, buf, strlen(buf) + 1);
-    buf = promptline("Author first name");
-    strncpy(first_name, buf, strlen(buf) + 1);
-    buf = promptline("Author last name");
-    strncpy(last_name, buf, strlen(buf) + 1);
-    int rc = db.execute(sql_insert, title, first_name, last_name);
-    if (!rc) {
+void BooksFindByTitle(SQLite& db) {
+    string title;
+    cout << "Find books by title > ";
+    getline(cin, title);
+
+    db.select(queryBooksFindByTitle, title.data());
+    DisplayData(db);
+}
+
+void BookAdd(SQLite& db) {
+    string title;
+    string first_name;
+    string last_name;
+    cout << "Add book:" << endl;
+    cout << "Title > ";
+    getline(cin, title);
+    cout << "Author first name > ";
+    getline(cin, first_name);
+    cout << "Author last name > ";
+    getline(cin, last_name);
+
+    if (!db.execute(queryBookAdd, title.data(), first_name.data(), last_name.data())) {
         db.error_message("Could not add row");
     }
 }
 
-void do_find_by_author(SQLite& db) {
-    const char* buf = promptline("Find by author last name");
-    db.select(sql_find_by_author, buf);
-    display_rows(db);
-}
-
-void do_find_by_title(SQLite& db) {
-    const char* buf = promptline("Find by title");
-    db.select(sql_find_by_title, buf);
-    display_rows(db);
-}
-
-void do_delete(SQLite& db) {
-    const char* id = nullptr;
-    puts("Delete book:");
-    id = promptline("Delete book with ID");
-    int rc = db.execute(sql_delete_by_id, id);
-    if (!rc) {
+void BookDelete(SQLite& db) {
+    string id;
+    cout << "Delete book with id > ";
+    getline(cin, id);
+    
+    if (!db.execute(queryBookDelete, id.data()))
         db.error_message("Could not delete row");
-    }
 }
 
-void do_drop_exit(SQLite& db) {
-    db.execute(sql_drop_table);
+void BooksDropTable(SQLite& db) {
+    db.execute(queryBooksDropTable);
     exit(0);
 }
 
 int main() {
-    const char* filename = nullptr;
-    puts("Create (or open if exists) database:");
-    filename = promptline("Database filename (default diary.db)");
-    size_t filename_size = strnlen(filename, MAX_SMALL_STRING_LENGTH);
-    if (filename_size == 0) {
-        filename = "diary.db";
+    string filename;
+    cout << "Create (or open if exists) database:" << endl;
+    cout << "Database filename (default books.db) > ";
+    getline(cin, filename);
+    if (filename.empty()) {
+        filename = "books.db";
     }
 
-    SQLite db(filename);
-    const char* rc = db.value(sql_check_book_table);
-    if (!rc) {
-        db.execute(sql_create_table);
+    SQLite db(filename.data());
+    if (!db.value(queryCheckBookTable)) {
+        db.execute(queryCreateTable);
     }
-    do_menu(db);
+
+    TextMenu(db);
 
     return 0;
 }
--- main.h
+++ main.h
@@ -1,25 +1,18 @@
 #ifndef SQL_CPP_MAIN_H
 #define SQL_CPP_MAIN_H
 
-#include "sqlite3-wrapper.h"
-#define MAX_SMALL_STRING_LENGTH 255
+#include "wrapper.h"
 
-void display_rows(SQLite & db);
+void TextMenu(SQLite&);
+void DisplayData(SQLite&);
 
-const char * promptline(const char * promt);
-void do_menu(SQLite & db);
-void do_jump(SQLite & db, const char response);
-
-void do_add(SQLite & db);
-void do_delete(SQLite & db);
-
-void do_list(SQLite& db);
-void do_list_by_author(SQLite & db);
-void do_list_by_title(SQLite & db);
-
-void do_find_by_author(SQLite & db);
-void do_find_by_title(SQLite & db);
-
-void do_drop_exit(SQLite & db);
+void Books(SQLite&);
+void BooksSortByAuthor(SQLite&);
+void BooksSortByTitle(SQLite&);
+void BooksFindByAuthor(SQLite&);
+void BooksFindByTitle(SQLite&);
+void BookAdd(SQLite&);
+void BookDelete(SQLite&);
+void BooksDropTable(SQLite&);
 
 #endif
